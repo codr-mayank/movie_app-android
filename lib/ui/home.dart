1bd9c71735ee238c1a78f4c4f99edd95892327c6
codr_mayank <gamblergamer9@gmail.com>
2020-05-24 16:08:19 +0530

    add list data and card tile view

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
 
 class MovieListView extends StatelessWidget {
+  final List movies = [
+    'Titanic',
+    'Blade Runner',
+    'Rambo',
+    'The Avengers',
+    'Avatar',
+    'I Am Legend',
+    '300',
+    'The Wolf of Wall Street',
+    'Interstellar',
+    'Game of Thrones',
+    'Vikings'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movies'),
+        backgroundColor: Colors.black12,
+      ),
+      backgroundColor: Colors.deepOrangeAccent.shade200,
+      body: ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              color: Colors.white,
+              child: ListTile(
+                title: Text(movies[index]),
+              ),
+            );
+          }),
+    );
   }
 }
