eceb13d93cca9fe114169e48216fbaab0ac3c605
codr-mayank <gamblergamer9@gmail.com>
2020-06-04 01:13:34 +0530

    details route - add bottom movie posters

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -184,7 +184,8 @@ class MovieListViewDetails extends StatelessWidget {
           MovieDetailsHeaderWithPoster(movie: movie),
           HorizontalLine(),
           MovieDetailsCast(movie: movie),
-          HorizontalLine()
+          HorizontalLine(),
+          MovieDetailsExtraPosters(posters: movie.images)
         ],
       ),
       // body: Center(
@@ -214,7 +215,7 @@ class MovieDetailsThumbnail extends StatelessWidget {
         Stack(alignment: Alignment.center, children: <Widget>[
           Container(
               width: MediaQuery.of(context).size.width,
-              height: 190,
+              height: 160,
               decoration: BoxDecoration(
                   image: DecorationImage(
                       image: NetworkImage(thumbnail), fit: BoxFit.cover))),
@@ -376,6 +377,45 @@ class HorizontalLine extends StatelessWidget {
         height: 0.5,
         color: Colors.grey,
       ),
+    );
+  }
+}
+
+class MovieDetailsExtraPosters extends StatelessWidget {
+  final List<String> posters;
+
+  const MovieDetailsExtraPosters({Key key, this.posters}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 8),
+          child: Text(
+            'More Movie Posters'.toUpperCase(),
+            style: TextStyle(fontSize: 14, color: Colors.white),
+          ),
+        ),
+        Container(
+            height: 170,
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              separatorBuilder: (context, index) => SizedBox(width: 8),
+              itemCount: posters.length,
+              itemBuilder: (context, index) => ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 4,
+                    height: 160,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(posters[index]),
+                            fit: BoxFit.cover)),
+                  )),
+            ))
+      ],
     );
   }
 }
